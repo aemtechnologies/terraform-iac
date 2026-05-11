7b6f16b5813a9c27d4901da5f3e87a42037dab7d
AEM Technologies GitHub Repository <65249478+aemtechnologies@users.noreply.github.com>
2026-05-11 22:01:40 +0530

    Secret Allowed
    Added AWS provider configuration and a new EC2 instance.

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,14 @@
-resource "aws_instance" "server1" {
-  ami           = "ami-062f0cc54dbfd8ef1"
-  instance_type = "t2.micro"
+provider "aws" {
+  region = "ap-south-1"
+  access_key = "AKIA3KJLEZFJ7HZL4EC2"
+  secret_key = "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"
+}
 
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0f559c3642608c138"
+  instance_type = "t2.micro"
+  
   tags = {
     Name = "server1"
-  }
+    }
 }
